cb36490fa7c92863123be8cc27bc2996f69c8224
GirishChauhan15 <girishchauhan333@mail.com>
2025-05-02 17:06:26 +0530

    Remove - ping.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,6 @@
 import server from './socket.js'
 import db_connect from "./db/index.js";
 import 'dotenv/config'
-import ping from './Ping.js';
 
 const port = process.env.PORT || 8000
 
@@ -11,7 +10,6 @@ db_connect().then(()=>{
     })
 
     server.listen(port, ()=>{
-        ping()
         // console.log(`Port is running at http://localhost:${port}`)
     })
 }).catch(err=>{
